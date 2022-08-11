5517e713a79c8cf96492d7af930d9a93f2311c18
Syd <svaughn4@stanford.edu>
2022-08-11 15:35:35 -0700

    Add click functions and final color touches

--- javascript.js
+++ javascript.js
@@ -1,3 +1,28 @@
-let pass = document.getElementById('confirmPass');
-pass.addEventListener('select', check());
+let form = document.getElementsByTagName('form')
+let check = document.getElementById('confirmPass');
+let pass = document.getElementById('pass'); 
+let message = document.getElementById('error')
+
+check.addEventListener('keyup', () => { 
+    if(check.value !== pass.value) { 
+        pass.style.borderColor='red'
+        check.style.borderColor='red' 
+        message.innerText='* Passwords do not match';
+        message.style.color='red'
+    } else { 
+        pass.style.borderColor='green'
+        check.style.borderColor='green'
+        message.innerText="";
+    }
+});
+
+
+
+function create() { 
+    let button = document.getElementById('create')
+    button.addEventListener('click', () => { 
+        
+    })
+}
+
 
